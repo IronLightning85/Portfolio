aeeba1d6ca9ff3f9aa215cec41fb4ed6bdf6e768
IronLightning85 <gochenaurbrennan@gmail.com>
2023-11-06 18:21:45 -0500

    finished up formatting

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// const navToggle = document.querySelector('nav-toggle');
-
-// navToggle.addEventListener('click', () => {
-//     document.body.classList.toggle('nav-open');
-// });
-
 function menu()
 {
     document.body.classList.toggle('nav-open');
+}
+
+function hideMenu()
+{
+document.body.classList.remove('nav-open');
 }
